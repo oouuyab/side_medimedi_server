b17c166b93d01ddc4d173ecdd337357b2eb396d7
oouuyab <oouuyab@gmail.com>
2022-04-08 00:42:55 +0900

    TypeORM DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts}'],
+      synchronize: process.env.ENVIRONMENT === 'local',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
