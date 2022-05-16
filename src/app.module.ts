a263c7afa11677d04b9bc7ce5c87ca601e421a94
oouuyab <oouuyab@gmail.com>
2022-05-16 22:04:49 +0900

    env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UserModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      username: process.env.DB_USER_NAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts}'],
-      synchronize: process.env.ENVIRONMENT === 'local',
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.prod.env' : '.env',
     }),
   ],
   controllers: [AppController],
